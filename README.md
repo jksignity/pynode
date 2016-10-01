ddd343fce5c488eb15656c7a40040eff24e9b14a
Jaskaran <jaskarans13@gmail.com>
2016-10-01 13:56:51 +0530

    finished readme, final for now

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# pynode
+PyNode
+=========
+
 small npm library for launching python workers and providing two way communication
+
+## Installation
+
+	npm install pynode --save
+
+## Usage
+
+##### Format is as follows, returns a Promise
+
+	var pynode = require('pynode');
+	pynode.exec(filename, arguments);
+
+##### 
+	
+	var pynode = require('pynode');
+	pynode.exec(filename, arguments)
+	.then(function(data){
+		data = data[0].replace(/'/g, '"').replace(/\r?\n|\r/g, '');
+		console.log(data);
+	})
+	.catch(function(err){
+		console.log(err)
+	})
+
+### Side Notes:
+
+* arguments are optional
+* if returning dictionaries or lists from python file, use json.dumps
+* removing \r,\n and "'" helps in better processing of output from python
+* \r, \n are present because data is still from stdout
+
+## Python files samples
+
+	please check examples directory for that.
+
+
+## Tests
+
+	npm test
+
+## Future Work
+
+* making it much better (still working)
+* allowing user to run python code directly from their file
+* filtering of inputs
+* maybe add more test cases, to test thoroughly
+
+## Contributing
+
+* All kinds of suggestions are welcome.
+* Collaboration also welcome to make it better or add more languages.
+* Please take care to maintain the existing coding style.
+* Add tests for any new functionalities.
+
+## Release History
+
+* 0.1.0 Initial release
--- test/index.js
+++ test/index.js
@@ -7,11 +7,11 @@ var chai = require('chai'),
 describe('Test runs', () => {
 
 	it('it should run the specified file and return contents', (done) => {
-		var res = pynode.exec(__dirname + '/../' + 'test.py');
+		var res = pynode.exec(__dirname + '/../examples/' + 'test.py');
 
 		res.then((d) => {
 			try{
-				d = d[0].replace(/\r?\n|\r/g, '').replace(/'/g, '"');
+				d = d[0].replace(/'/g, '"').replace(/\r?\n|\r/g, '');
 				expect(d).to.equal('hello');
 				done();
 			}catch(e){
@@ -21,28 +21,14 @@ describe('Test runs', () => {
 
 	});
 
-	it('it should run the w/o directory name and return contents', (done) => {
-		var res = pynode.exec('test.py');
-
-		res.then((d) => {
-			try{
-				d = d[0].replace(/\r?\n|\r/g, '').replace(/'/g, '"');
-				expect(d).to.equal('hello');
-				done();
-			}catch(e){
-				done(e);
-			}
-		});
-
-	});
 
 	it('it should return the array of print statements from python', (done) => {
-		var res = pynode.exec('second.py');
+		var res = pynode.exec(__dirname + '/../examples/' + 'second.py');
 
 		res.then((d) => {
 			const shouldbe = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 			try{
-				d = d[0].replace(/\r?\n|\r/g, '').replace(/'/g, '"');
+				d = d[0].replace(/'/g, '"').replace(/\r?\n|\r/g, '');
 				d = JSON.parse(d);
 				expect(d).to.be.instanceof(Array);
 				expect(d).to.deep.equal(shouldbe);
@@ -55,10 +41,28 @@ describe('Test runs', () => {
 	});
 
 	it('should fail with no file found', (done) => {
-		expect(pynode.exec('somename.py'))
+		expect(pynode.exec(__dirname + '/../examples/' + 'somename.py'))
 			.to.be.rejected;
 			// .to.be.an.instanceof('Error');
 		done();
-	})
+	});
+
+	it('should return my sent arguments', (done) => {
+		var args = ['foo', 'bar', 'baz'];
+		var res = pynode.exec(__dirname + '/../examples/' + 'third.py', args);
+
+		res.then((d) => {
+			const shouldbe = ['foo', 'bar', 'baz'];
+			try{
+				d = d[0].replace(/'/g, '"').replace(/\r?\n|\r/g, '');
+				d = JSON.parse(d);
+				expect(d).to.be.instanceof(Array);
+				expect(d).to.deep.equal(shouldbe);
+				done();
+			}catch(e){
+				done(e);
+			}
+		})
+	});
 
 })
